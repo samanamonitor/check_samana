ceb03c296d77d970775ce8f10681b8aaeead1773
Fabian Baena <fabian.baena@samanagroup.co>
2020-11-10 10:45:17 -0500

    added warning and critical thresholds

--- src/check_winrm.py
+++ src/check_winrm.py
@@ -140,7 +140,7 @@ to get a license.
 Copyright (c) 2019 Samana Group LLC
 
 Usage:
-  check_winrm.py -H <host name> < -d <user domain name> -u <username> -p <password> | -a <auth file> > -n <nagios>
+  check_winrm.py -H <host name> < -d <user domain name> -u <username> -p <password> | -a <auth file> > -n <nagios> [-w <name resolution time warn (ms)>,<ping rtt warn (ms)>,<ping packet loss %>,<winrm time warn (ms)>] [-c <name resolution crit (ms)><ping rtt crit (ms)>,<ping packet loss %>,<warn time crit (ms)>]
   check_winrm -h
 
   <host domain name> Session Host server domain name
@@ -150,6 +150,10 @@ Usage:
   <password>         password of the user with privileges in the Citrix Farm
   <auth file>        file name containing user's credentials
   <nagios>           Nagios server IP
+  <name resolution>  Time to resolve the host name in miliseconds. Only integer values accepted.
+  <ping rtt>         Ping round trip time in miliseconds. Only integer values accepted.
+  <ping packet loss> \% of packets that can be lost. Don't use \% sign in value
+  <winrm time>       WinRM execution time in miliseconds. Only integer values accepted.
 """
   print(usage)
 
@@ -158,21 +162,27 @@ def get_dns_ip(hn):
 
   pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
   if pat.match(hn):
-    return hn
+    return (hn, 0)
 
   try:
+    dns_start = time()
     server_data = socket.gethostbyname_ex(hn)
+    ips = server_data[2]
+    if not isinstance(ips, list) and len(ips) != 1:
+      raise ValueError("hostname is linked to more than 1 IP or 0 IPs")
+    dns_end = time()
+    return (ips[0], dns_end-dns_start)
+
+  except ValueError as err:
+    print "CRITICAL - %s" % str(err)
+    exit(2)
+  except IndexError as err:
+    print "CRITICAL - invalid data received from gethostbyname_ex %s\n%s" % (str(err), server_data)
+    exit(2)    
   except Exception as err:
-    print "CRITICAL - Unable to resove hostname to IP address"
+    print "CRITICAL - Unable to resove hostname to IP address\n%s" % str(err)
     exit(2)
-  if len(server_data) != 3:
-    print "CRITICAL - invalid data received from gethostbyname_ex %s" % server_data
-    exit(2)
-  ips = server_data[2]
-  if not isinstance(ips, list) and len(ips) != 1:
-    print "CRITICAL - hostname is linked to more than 1 IP or 0 IPs"
-    exit(2)
-  return ips[0]
+
 
 def ping_host(ip):
   import subprocess
@@ -186,20 +196,28 @@ def ping_host(ip):
   }
   p = subprocess.Popen(["ping", "-c", "3", ip], stdout = subprocess.PIPE)
   out = p.communicate()
-  pat = re.search("^(\d+) packets transmitted, (\d+) received", out[0], flags=re.M)
-  if pat is None:
-    print "UNKOWN - ping output invalid %s" % out[0]
-    exit(3)
-  packets = pat.groups()
-  data['packets_sent'] = packets[0]
-  data['packets_received'] = packets[1]
+  try:
+    pat = re.search("^(\d+) packets transmitted, (\d+) received", out[0], flags=re.M)
+    if pat is None:
+      raise ValueError("Cannot extract packets from ping output.")
+    packets = pat.groups()
 
-  pat = re.search("^rtt min/avg/max/mdev = ([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)", out[0], flags=re.M)
-  rtt = pat.groups()
-  data['min'] = float(rtt[0])
-  data['avg'] = float(rtt[1])
-  data['max'] = float(rtt[2])
-  data['mdev'] = float(rtt[3])
+    pat = re.search("^rtt min/avg/max/mdev = ([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)", out[0], flags=re.M)
+    if pat is None:
+      raise ValueError("Cannot extract ping rtt times.")
+    rtt = pat.groups()
+
+    data['packets_sent'], = int(packets[0])
+    data['packets_received'] = int(packets[1])
+    data['min'] = int(float(rtt[0]))
+    data['avg'] = int(float(rtt[1]))
+    data['max'] = int(float(rtt[2]))
+    data['mdev'] = int(float(rtt[3]))
+  except (ValueError, IndexError) as e:
+    print "UNKNOWN - ping output invalid %s\n%s" % (str(e), out[0])
+    exit(3)
+  except Exception as e:
+    print "UNKNOWN - unexpected error %s" % str(e)
   return data
 
 def main():
@@ -215,10 +233,18 @@ def main():
   authfile = None
   load_from_server = False
   nagiosaddress = None
+  warning = None
+  critical = None
+  ping_warn = None
+  ping_crit = None
+  winrm_warn = None
+  winrm_crit = None
+  dns_warn = None
+  dns_crit = None
   script = 'samanamon.ps1'
 
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "H:d:u:p:ha:n:s:")
+    opts, args = getopt.getopt(sys.argv[1:], "H:d:u:p:ha:n:s:w:c:")
 
     for o, a in opts:
       if o == '-H':
@@ -235,6 +261,10 @@ def main():
         nagiosaddress = a
       elif o == '-s':
         script = a
+      elif o == '-w':
+        warning = a
+      elif o == '-c':
+        critical = a
       elif o == '-h':
         raise Exception("Unknown argument")
 
@@ -251,21 +281,78 @@ def main():
       print "UNKNOWN - Invalid authentication information"
       exit(3)
 
-    dns_start = time()
-    hostip = get_dns_ip(hostaddress)
-    dns_end = time()
+    if warning is not None:
+      try:
+        (dns_warn, ping_warn, packet_loss_warn, winrm_warn) = warning.split(',')
+        dns_warn = int(dns_warn)
+        ping_warn = int(ping_warn)
+        packet_loss_warn = int(packet_loss_warn)
+        winrm_warn = int(winrm_warn)
+      except ValueError:
+        print "UNKNOWN - Invalid Warning values"
+        usage()
+        exit(3)
+    if critical is not None:
+      try:
+        (dns_crit, ping_crit, packet_loss_crit, winrm_crit) = critical.split(',')
+        dns_crit = int(dns_crit)
+        ping_crit = int(ping_crit)
+        packet_loss_crit = int(packet_loss_crit)
+        winrm_crit = int(winrm_crit)
+      except ValueError:
+        print "UNKNOWN - Invalid Critical values"
+        usage()
+        exit(3)
+
+    (hostip, dns_time) = get_dns_ip(hostaddress)
     ping_data = ping_host(hostip)
 
-    client = WinRMScript(hostaddress, user_auth, nagiosaddress)
     winrm_start = time()
+    client = WinRMScript(hostaddress, user_auth, nagiosaddress)
     out = client.get(script)
-    winrm_end = time()
+    winrm_time = time() - winrm_start
 
-    perc_packet_loss = 100-int(100.0 * float(ping_data['packets_received']) / float(ping_data['packets_sent']))
-    winrm_time = winrm_end - winrm_start
-    print "OK - Data Collected | ping_perc_packet_loss=%d;;;; ping_rtt=%fms;;;; winrm_time=%f;;;;\n%s" % (
-      perc_packet_loss, ping_data['avg'], winrm_time, out)
-    return 0
+    perc_packet_loss = 100-int(100.0 * ping_data['packets_received'] / ping_data['packets_sent'])
+
+    perf_data = "dns_resolution=%d;;;; ping_perc_packet_loss=%d;;;; ping_rtt=%d;;;; winrm_time=%d;;;;" %
+    (dns_time, perc_packet_loss, ping_data['avg'], winrm_time)
+
+    if dns_crit is not None and dns_crit < dns_time:
+      print "CRITICAL - DNS name resolution took longer than expected %d | %s\n%s." %
+        (dns_time, perf_data, out)
+      exit(2)
+    if dns_warn is not None and dns_warn < dns_time:
+      print "WARNING - DNS name resolution took longer than expected %d | %s\n%s." %
+        (dns_time, perf_data, out)
+      exit(1)
+    if packet_loss_crit is not None and packet_loss_crit < perc_packet_loss:
+      print "CRITICAL - PING lost %d\% packets | %s\n%s" %
+        (perc_packet_loss, perf_data, out)
+      exit(2)
+    if packet_loss_warn is not None and packet_loss_warn < perc_packet_loss:
+      print "WARNING - PING lost %d\% packets | %s\n%s" %
+        (perc_packet_loss, perf_data, out)
+      exit(1)
+    if ping_crit is not None and ping_crit < ping_data['avg']:
+      print "CRITICAL - PING rtt is greater than expected %d ms | %s\n%s" %
+        (ping_data['avg'], perf_data, out)
+      exit(2)
+    if ping_warn is not None and ping_warn < ping_data['avg']:
+      print "WARNING - PING rtt is greater than expected %d ms | %s\n%s" %
+        (ping_data['avg'], perf_data, out)
+      exit(1)
+    if winrm_crit is not None and winrm_crit < winrm_time:
+      print "CRITICAL - WinRM took longer than expected %d ms | %s\n%s" %
+        (winrm_time, perf_data, out)
+      exit(2)
+    if winrm_warn is not None and winrm_warn < winrm_time:
+      print "WARNING - WinRM took longer than expected %d ms | %s\n%s" %
+        (winrm_time, perf_data, out)
+      exit(1)
+
+    print "OK - Data Collected | %s\n%s" % 
+        (winrm_time, perf_data, out)
+    exit(0)
 
   except Exception as err:
     print "UNKNOWN - main Error: " + str(err)
