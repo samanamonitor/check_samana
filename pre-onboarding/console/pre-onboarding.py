b369d8084f66b8729c1e5f847dd1fb74fb4ba16c
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-07 15:29:33 -0500

    added ldap search functions

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -78,6 +78,7 @@ def get_printers(xmltxt):
     for k,v in pairwise(root[0]):
         if k.text == "printers" and v.attrib['Type'] == "System.Object[]":
             for printer in v:
+                print v
                 printers += xml_2_hash(v)
             break
     return printers
