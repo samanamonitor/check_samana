b5f98a6bbceb96243c377c5ce2b7b03aa1c326eb
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-07 10:29:26 -0500

    fixed bug

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -4,7 +4,7 @@ def application(environ, start_fn):
     indata=environ['PATH_INFO'].split('/')
     if len(indata > 1):
         username=indata[2]
-    else
+    else:
         username=None
     start_fn('200 OK', [('Content-Type', 'text/plain')])
     return ["Hello World!\n<br>%s" % username]
