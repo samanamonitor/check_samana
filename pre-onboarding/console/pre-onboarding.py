790bfbabeb54b4dc9b850c21cd810fd1ec56da11
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-07 10:20:56 -0500

    fixed bug

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
 def application(environ, start_fn):
-    start_fn('200 OK', [('Content-Type', 'text/plain')])
+    start_fn('200 OK %s' % environ, [('Content-Type', 'text/plain')])
     return ["Hello World!\n"]
