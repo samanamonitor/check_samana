6234709969a5b999fb7ec33db935c7d7e8c4e694
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-07 10:26:53 -0500

    fixed bug

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
 def application(environ, start_fn):
+    indata=environ['PATH_INFO'].split('/')
     start_fn('200 OK', [('Content-Type', 'text/plain')])
-    return ["Hello World!\n<br>%s" % environ['PATH_INFO']]
+    return ["Hello World!\n<br>%s" % indata]
