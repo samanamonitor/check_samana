cf790a2060bef00c4b7e8cb3d9be21682c202677
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-07 10:37:40 -0500

    fixed bug

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python
 
+import json
+
 def application(environ, start_fn):
     indata=environ['PATH_INFO'].split('/')
     if len(indata) > 1:
         username=indata[1]
     else:
-        username=None
-    start_fn('200 OK', [('Content-Type', 'text/plain')])
+        username=''
+    if username == '':
+        start_fn('200 OK', [('Content-Type', 'text/html')])
+        return [query_page()]
+    else:
+        start_fn('200 OK', [('Content-Type', 'application/json')])
+        return [json.dumps({'username': username})]
     return ["Hello World!\n<br>%s" % username]
+
+def query_page():
+    return '''
+<HTML>
+<HEAD><TITLE>Pre-Onboarding Console</TITLE>
+<BODY>
+    <H1><CENTER>Pre-Onboarding Console</CENTER></H1>
+</BODY>
+</HTML>
+'''
