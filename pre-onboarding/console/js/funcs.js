ad77f8c6599eb58f66b5195d8033fe6ad1922882
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-08 09:58:15 -0500

    fix bugs

--- pre-onboarding/console/js/funcs.js
+++ pre-onboarding/console/js/funcs.js
@@ -6,6 +6,6 @@ function show_users(data) {
     u = $($("#usertemplate").html());
     user_name = u.find(".user-name");
     user_name.text("hello");
-    user_name.attr("sid") = "asdf";
+    user_name.attr("sid", "asdf");
     $("#userlist").append(u)
 }
