710bd2425e558877593e6e66ee3e64fb39d721c2
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-08 11:36:15 -0500

    fix bugs

--- pre-onboarding/console/js/funcs.js
+++ pre-onboarding/console/js/funcs.js
@@ -3,12 +3,15 @@ function load_users() {
 }
 
 function show_users(data) {
-    u = $($("#usertemplate").html());
-    user_name = u.find(".user-name");
-    user_name.text("hello");
-    user_name.attr("sid", "asdf");
-    user_name.click(show_data)
-    $("#userlist").append(u)
+    for(i=0; i<data.length(); i++) {
+        u = $($("#usertemplate").html());
+        user_name = u.find(".user-name");
+        user_name.text(data[i]['samaccountname']);
+        user_name.attr("sid", data[i]['sid']);
+        user_name.click(show_data)
+        $("#userlist").append(u)
+
+    }
 }
 
 function show_data(e) {
