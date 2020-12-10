c9370b3934a6d912a5f15547b3a4fbbb60450d01
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-10 09:51:16 -0500

    fixed bug

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -129,7 +129,7 @@ def get_drives(xmltxt):
                 drives.append(xml_2_hash(drive))
             break
         else:
-            print v
+            print v.attrib['Name']
             drives.append(xml_2_hash(v))
     return drives
 
