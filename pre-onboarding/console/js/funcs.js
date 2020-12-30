8427cebdcc6c34c997e136e8978961ef6f86778a
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-30 10:05:42 -0500

    add multiuser

--- pre-onboarding/console/js/funcs.js
+++ pre-onboarding/console/js/funcs.js
@@ -6,7 +6,7 @@ function show_users(data) {
     for(i=0; i<data.length; i++) {
         u = $($("#usertemplate").html());
         user_name = u.find(".user-name");
-        user_name.text(data[i]['samaccountname']);
+        user_name.text(data[i]['samaccountname'] + " - " + data[i]['displayName']);
         user_name.attr("sid", data[i]['sid']);
         user_name.click(show_data);
         $("#userlist").append(u)
