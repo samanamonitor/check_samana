f0c6f41a47b0472d71ee521c864da8d12c631036
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-30 09:27:12 -0500

    add multiuser

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -301,7 +301,7 @@ def get_user_xmldata(objectSid):
 
     import base64
     data = base64.b64decode(data_b64)
-    return data.decode("UTF-16").encode("UTF-8")
+    return data.decode("UTF-16").encode("ascii")
 
 def get_user_sid(data):
     if len(data.split('-')) == 8:
