21f0b2e18f073aaccf0cb9ca17a984f962d521e3
Fabian Baena <fabian.baena@samanagroup.co>
2020-12-30 09:14:53 -0500

    add multiuser

--- pre-onboarding/console/pre-onboarding.py
+++ pre-onboarding/console/pre-onboarding.py
@@ -157,7 +157,7 @@ def get_listusers(output="array"):
         sid_list = []
         etcd_sid_list = client.get('/pre-onboarding').children
         for item in etcd_sid_list:
-            sid_list.append(item.key.split('/')[-1].decode("UTF-16").encode("UTF-8"))
+            sid_list.append(item.key.split('/')[-1].encode("ascii"))
     except etcd.EtcdKeyNotFound:
         raise etcd.EtcdKeyNotFound('400 NO USERS FOUND', "No users found")
 
