1bf455ea503b3860bf045cc372e818362b91e9a2
Fabian Baena <fabian.baena@samanagroup.co>
2021-02-25 14:48:42 -0500

    added file

--- nagios/src/check_ctx2_farm.py
+++ nagios/src/check_ctx2_farm.py
@@ -34,7 +34,7 @@ class CitrixXD:
       http = urllib3.PoolManager()
       r = http.request('GET', 'http://localhost:2379' + path)
       print 'http://localhost:2379' + path
-      print r.status
+      print r.status == 200
       if r.status != '200': raise KeyError
       self.data =json.loads(r.data)
       age_secs = time.time() - self.data['epoch']
