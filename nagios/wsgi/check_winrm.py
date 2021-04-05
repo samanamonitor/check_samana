ab3e31f7efbc89433eeeb1d1370afae9cc5af650
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-05 18:08:17 -0400

    added file

--- nagios/wsgi/check_winrm.py
+++ nagios/wsgi/check_winrm.py
@@ -1,3 +1,93 @@
+from winrm.protocol import Protocol
+from base64 import b64encode
+import json
+import sys, getopt
+import time
+import random
+import unicodedata
+import re
+from time import time
+
+class WinRMScript:
+  def __init__(self, hostaddress, auth):
+    try:
+      if auth is None:
+        raise Exception("Authentication data missing")
+      if 'domain' not in auth or auth['domain'] is None:
+        raise Exception("The user domain name is a mandatory argument")
+      if 'username' not in auth or auth['username'] is None:
+        raise Exception("The username is a mandatory argument")
+      if 'password' not in auth or auth['password'] is None:
+        raise Exception("The password is a mandatory argument")
+
+    except Exception as e:
+      print "UNKNOWN - Error " + str(e)
+      usage()
+      exit(3)
+
+    self.data = {}
+    self.hostaddress = hostaddress
+    if 'upn' in auth:
+      self.username = auth['username']
+    else:
+      self.username = auth['domain'] + '\\' + auth['username']
+    self.password = auth['password']
+
+  def run(self, scripturl, scriptarguments):
+    scriptpath = "c:\\samanamon"
+    #scripturl="http://%s/%s" % (self.nagiosaddress, scriptname)
+    scriptname = scripturl.split('/')[-1]
+    script = '''
+if (-Not (Test-Path %(scriptpath)s)) { mkdir %(scriptpath)s | Out-Null}
+"Environment prepared." | Out-Host
+Invoke-WebRequest -Uri %(scripturl)s -OutFile "%(scriptpath)s\\%(scriptname)s"
+if (-Not (Test-Path %(scriptpath)s\\%(scriptname)s)) { 
+  "File not downloaded" | Out-Host; 
+  Remove-Item -Recurse -Force %(scriptpath)s
+  exit 1 
+}
+"Downloaded Script." | Out-Host
+%(scriptpath)s\\%(scriptname)s %(scriptarguments)s| Out-Host
+"Done executing script" | Out-Host
+del %(scriptpath)s\\%(scriptname)s
+Remove-Item -Recurse -Force %(scriptpath)s
+"Done cleanup" | Out-Host
+''' % { 'scripturl': scripturl, 
+      'scriptpath': scriptpath, 
+      'scriptname': scriptname,
+      'scriptarguments': scriptarguments,
+      'hostaddress': self.hostaddress
+      }
+
+    shell_id = None
+    command_id = None
+    p = None
+    error = 0
+    std_out = ''
+    std_err = ''
+    try:
+      p = Protocol(
+        endpoint='http://%s:5985/wsman' % self.hostaddress,
+        transport='ntlm',
+        username=self.username,
+        password=self.password,
+        server_cert_validation='ignore')
+      shell_id = p.open_shell()
+      encoded_ps = b64encode(script.encode('utf_16_le')).decode('ascii')
+      command_id = p.run_command(shell_id, 'powershell', ['-encodedcommand {0}'.format(encoded_ps), ])
+      std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
+      if status_code != 0:
+        print("ERROR - %s" % std_err)
+        error = 1
+    except Exception as e:
+      print("UNKNOWN - Unable to get data from Server (%s) %s." % (str(e), type(e).__name__))
+      error = 3
+    finally:
+      p.cleanup_command(shell_id, command_id)
+      p.close_shell(shell_id)
+    if error > 0: exit(error)
+    return std_out
+
 # The application interface is a callable object
 def application ( # It accepts two arguments:
     # environ points to a dictionary containing CGI like environment
@@ -11,7 +101,7 @@ def application ( # It accepts two arguments:
 
     # Build the response body possibly
     # using the supplied environ dictionary
-    response_body = 'Request method: %s' % environ['REQUEST_METHOD']
+    response_body = 'Request method: %s \n%s' % (environ['REQUEST_METHOD'], environ)
 
     # HTTP response code and message
     status = '200 OK'
