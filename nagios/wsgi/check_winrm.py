223810b12a1d7e3889fb469d6785bac57f25d797
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-05 17:59:23 -0400

    added file

---
+++ nagios/wsgi/check_winrm.py
@@ -0,0 +1,32 @@
+# The application interface is a callable object
+def application ( # It accepts two arguments:
+    # environ points to a dictionary containing CGI like environment
+    # variables which is populated by the server for each
+    # received request from the client
+    environ,
+    # start_response is a callback function supplied by the server
+    # which takes the HTTP status and headers as arguments
+    start_response
+):
+
+    # Build the response body possibly
+    # using the supplied environ dictionary
+    response_body = 'Request method: %s' % environ['REQUEST_METHOD']
+
+    # HTTP response code and message
+    status = '200 OK'
+
+    # HTTP headers expected by the client
+    # They must be wrapped as a list of tupled pairs:
+    # [(Header name, Header value)].
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(response_body)))
+    ]
+
+    # Send them to the server using the supplied function
+    start_response(status, response_headers)
+
+    # Return the response body. Notice it is wrapped
+    # in a list although it could be any iterable.
+    return [response_body]
