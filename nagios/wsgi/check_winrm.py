d359fee275d1ebc24c4e92636344c8b7b6406565
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-05 19:21:12 -0400

    added file

--- nagios/wsgi/check_winrm.py
+++ nagios/wsgi/check_winrm.py
@@ -10,6 +10,12 @@ from time import time
 from cgi import parse_qs, escape
 
 
+class CheckWinRMExceptionWARN(Exception):
+    pass
+
+class CheckWinRMExceptionCRIT(Exception):
+    pass
+
 class WinRMScript:
   def __init__(self, hostaddress, auth):
     try:
@@ -90,6 +96,43 @@ Remove-Item -Recurse -Force %(scriptpath)s
     if error > 0: exit(error)
     return std_out
 
+def get_dns_ip(hn):
+  import socket
+
+  pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+  if pat.match(hn):
+    return (hn, 0)
+
+  try:
+    dns_start = time()
+    server_data = socket.gethostbyname_ex(hn)
+    ips = server_data[2]
+    if not isinstance(ips, list) and len(ips) != 1:
+      raise ValueError("hostname is linked to more than 1 IP or 0 IPs")
+    dns_time = (time() - dns_start) * 1000
+    return (ips[0], dns_time)
+
+  except ValueError as err:
+    raise CheckWinRMExceptionCRIT(str(err))
+
+  except IndexError as err:
+    raise CheckWinRMExceptionCRIT("Invalid data received from gethostbyname_ex %s\n%s" % (str(err), server_data))
+
+  except Exception as err:
+    raise CheckWinRMExceptionCRIT("Unable to resove hostname to IP address\n%s" % str(err))
+
+def auth(username, domainname, password, authfile):
+    if authfile is not None:
+        return auth_file(authfile)
+
+    if username is None or domainname is None or password is None:
+        return None
+
+    return {
+        'username': username,
+        'domain': domainname,
+        'password': password
+        }
 
 def application ( environ, start_response):
 
@@ -115,25 +158,114 @@ def application ( environ, start_response):
         if data['hostaddress'] is None:
             raise Exception("Invalid Host address")
 
-        response_body = 'Request method: %s \n%s' % (environ['REQUEST_METHOD'], data)
+        if nagiosaddress is None and url is None:
+            raise Exception("UNKNOWN - Powershell script location not defined.")
+
+        if url is None:
+            url = "http://%s/%s" % (nagiosaddress, script)
+        #TODO check URL syntax for the case when we receive URL from user
+
+        user_auth = auth(username, u_domain, password, authfile)
+        if user_auth is None:
+            raise Exception("UNKNOWN - Invalid authentication information")
+
+        if warning is not None:
+            try:
+                (dns_warn, ping_warn, packet_loss_warn, winrm_warn) = warning.split(',')
+                dns_warn = int(dns_warn)
+                ping_warn = int(ping_warn)
+                packet_loss_warn = int(packet_loss_warn)
+                winrm_warn = int(winrm_warn)
+            except ValueError:
+                raise Exception("UNKNOWN - Invalid Warning values")
+
+        if critical is not None:
+            try:
+                (dns_crit, ping_crit, packet_loss_crit, winrm_crit) = critical.split(',')
+                dns_crit = int(dns_crit)
+                ping_crit = int(ping_crit)
+                packet_loss_crit = int(packet_loss_crit)
+                winrm_crit = int(winrm_crit)
+            except ValueError:
+                raise Exception("UNKNOWN - Invalid Critical values")
+
+        (hostip, dns_time) = get_dns_ip(hostaddress)
+        ping_data = ping_host(hostip)
+
+        winrm_start = time()
+        client = WinRMScript(hostaddress, user_auth)
+        out = client.run(url, scriptarguments)
+        winrm_time = (time() - winrm_start) * 1000
+
+        perc_packet_loss = 100-int(100.0 * ping_data['packets_received'] / ping_data['packets_sent'])
+
+        perf_data = "dns_resolution=%d;%s;%s;; ping_perc_packet_loss=%d;%s;%s;; ping_rtt=%d;%s;%s;; winrm_time=%d;%s;%s;;" % \
+            (dns_time, dns_warn if dns_warn is not None else '', dns_crit if dns_crit is not None else '',
+                perc_packet_loss, packet_loss_warn if packet_loss_warn is not None else '', packet_loss_crit if packet_loss_crit is not None else '',
+                ping_data['avg'], ping_warn if ping_warn is not None else '', ping_crit if ping_crit is not None else '', 
+                winrm_time, winrm_warn if winrm_warn is not None else '', winrm_crit if winrm_crit is not None else '')
+
+        if dns_crit is not None and dns_crit < dns_time:
+            raise CheckWinRMExceptionCRIT("DNS name resolution took longer than expected %d | %s\n%s." % \
+                (dns_time, perf_data, out))
+
+        if dns_warn is not None and dns_warn < dns_time:
+            raise CheckWinRMExceptionWARN("DNS name resolution took longer than expected %d | %s\n%s." % \
+            (dns_time, perf_data, out))
+
+        if packet_loss_crit is not None and packet_loss_crit < perc_packet_loss:
+            raise CheckWinRMExceptionCRIT("PING lost %d\% packets | %s\n%s" % \
+            (perc_packet_loss, perf_data, out))
+
+        if packet_loss_warn is not None and packet_loss_warn < perc_packet_loss:
+            raise CheckWinRMExceptionWARN("PING lost %d\% packets | %s\n%s" % \
+            (perc_packet_loss, perf_data, out))
+
+        if ping_crit is not None and ping_crit < ping_data['avg']:
+            raise CheckWinRMExceptionCRIT("PING rtt is greater than expected %d ms | %s\n%s" % \
+            (ping_data['avg'], perf_data, out))
+
+        if ping_warn is not None and ping_warn < ping_data['avg']:
+            raise CheckWinRMExceptionWARN("PING rtt is greater than expected %d ms | %s\n%s" % \
+            (ping_data['avg'], perf_data, out))
+
+        if winrm_crit is not None and winrm_crit < winrm_time:
+            raise CheckWinRMExceptionCRIT("WinRM took longer than expected %d ms | %s\n%s" % \
+            (winrm_time, perf_data, out))
+
+        if winrm_warn is not None and winrm_warn < winrm_time:
+            raise CheckWinRMExceptionWARN("WinRM took longer than expected %d ms | %s\n%s" % \
+            (winrm_time, perf_data, out))
+
+        response_body = json.dumps({
+            'status' = 0,
+            'message' = "OK - Data Collected | %s\n%s" % (perf_data, out)
+            })
 
 
-        status = '200 OK'
-        response_headers = [
-            ('Content-Type', 'application/json'),
-            ('Content-Length', str(len(response_body)))
-        ]
+    except CheckWinRMExceptionWARN as e:
+        response_body = json.dumps({
+            'status': 1,
+            'message': "WARNING - %s" % e
+            })
+
+    except CheckWinRMExceptionCRIT as e:
+        response_body = json.dumps({
+            'status': 2,
+            'message': "CRITICAL - " % e
+            })
 
     except Exception as e:
-        status = "400 Bad Request"
-        msg = "UNKNOWN"
-        response_body = "Error: %s" % e
-        response_headers = [
-            ('Content-Type', 'text/plain'),
-            ('Content-Length', str(len(response_body)))
-        ]
+        response_body = json.dumps({
+            'status': 3,
+            'message': "UNKNOWN - %s" % e
+            })
 
-
+    status = '200 OK'
+    response_headers = [
+        ('Content-Type', 'application/json'),
+        ('Content-Length', str(len(response_body)))
+    ]
 
     start_response(status, response_headers)
 
