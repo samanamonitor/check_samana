ce93fa6e24c85ad38c5c77f790ab79fb301de022
Fabian Baena <fabian.baena@samanagroup.co>
2021-04-19 10:23:11 -0400

    add ttl to etcd request

--- nagios/winsrc/samanamonctx.ps1
+++ nagios/winsrc/samanamonctx.ps1
@@ -63,7 +63,7 @@ Get-BrokerMachine -MaxRecordCount 5000 | ForEach {
     $_ | Add-Member -NotePropertyName epoch -NotePropertyValue 0
     $_.epoch = $epoch
     $value = $_ | ConvertTo-JSON -Compress
-    $res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value, ttl=300} `
+    $res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value; ttl=300} `
         -uri "$($SamanaMonitorURI)/v2/keys/samanamonitor/ctx_data/$($ComputerName)/hosts/$($_.DnsName.ToLower())" `
         -ContentType "application/x-www-form-urlencoded"
 }
@@ -81,7 +81,7 @@ $DesktopGroup.Keys | ForEach {
 $Farm["epoch"] = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
 $Farm['LoadIndex'] /= $Farm['TotalServers']
 $value = $Farm | ConvertTo-JSON -Compress
-$res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value, ttl=300} `
+$res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value; ttl=300} `
     -uri "$($SamanaMonitorURI)/v2/keys/samanamonitor/ctx_data/$($ComputerName)/farm" `
     -ContentType "application/x-www-form-urlencoded"
 
