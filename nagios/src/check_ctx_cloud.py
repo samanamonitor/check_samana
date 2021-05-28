b179664e21c5e9220cbc2cdd0f089bd8daf7bfe7
Fabian Baena <fabian.baena@samanagroup.co>
2021-05-28 14:54:36 -0400

    debug

--- nagios/src/check_ctx_cloud.py
+++ nagios/src/check_ctx_cloud.py
@@ -120,9 +120,6 @@ def main(argv):
         exit(3)
 
     print("OK - Data Collected")
-    customer_id='oopvmnxxhaqv'
-    client_id='f2d83ca7-eae0-4515-b531-fbeddbe68948'
-    client_secret='RCLrOBZo5y8MTpa0qyBPsA=='
 
 if __name__ == "__main__":
   main(sys.argv)
