4bffcb8e8f3a340ccbca121d0fbf20deda58b536
Fabian Baena <fabian.baena@samanagroup.co>
2022-01-21 16:09:32 -0500

    fix bug

--- nagios/winsrc/samanamonctx.ps1
+++ nagios/winsrc/samanamonctx.ps1
@@ -92,9 +92,11 @@ Get-BrokerMachine -MaxRecordCount 5000 | ForEach {
     $_ | Add-Member -NotePropertyName epoch -NotePropertyValue 0
     $_.epoch = $epoch
     $value = $_ | ConvertTo-JSON -Compress
-    $res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value; ttl=$ttl} `
-        -uri "$($SamanaMonitorURI)/v2/keys/samanamonitor/ctx_data/$($ComputerName)/hosts/$($_.DnsName.ToLower())" `
-        -ContentType "application/x-www-form-urlencoded"
+    if ($_.DnsName -ne "" ) {
+        $res = Invoke-WebRequest -UseBasicParsing -Method "PUT" -Body @{value=$value; ttl=$ttl} `
+            -uri "$($SamanaMonitorURI)/v2/keys/samanamonitor/ctx_data/$($ComputerName)/hosts/$($_.DnsName.ToLower())" `
+            -ContentType "application/x-www-form-urlencoded"
+    }
 }
 
 $DesktopGroup.Keys | ForEach {
