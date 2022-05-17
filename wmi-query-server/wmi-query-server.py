021bbcc4f0e54092fa24742147dcee42314ebf70
Fabian Baena <fabian.baena@samanagroup.co>
2022-05-17 14:13:29 -0400

    fix bug

--- wmi-query-server/wmi-query-server.py
+++ wmi-query-server/wmi-query-server.py
@@ -220,12 +220,13 @@ def application (environ, start_response):
         res['perf2'] = ""
 
     response_body = json.dumps(res)
-    print(response_body)
+
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'application/json'),
         ('Content-Length', str(len(response_body)))
     ]
+    print(response_body)
     start_response(status, response_headers)
 
     return [response_body]
