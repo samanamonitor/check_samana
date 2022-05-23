7289bbae5c1423a32acfe709abed75e4fb1fa70f
Fabian Baena <fabian.baena@samanagroup.co>
2022-05-23 12:17:57 -0400

    fix bug

--- nagios/src/check_wmi_wsgi.py
+++ nagios/src/check_wmi_wsgi.py
@@ -7,7 +7,7 @@ import sys, getopt
 import traceback
 from samana.nagios import CheckUnknown, CheckWarning, CheckCritical, CheckResult
 from samana.base import get_dns_ip, ping_host, perf, auth_file
-import urllib
+import urllib.request
 
 event_level = 2
 event_secs = 300
