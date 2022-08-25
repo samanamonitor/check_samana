7d64bf5402bbd920ad7ef1cc23f3a9e24e768a9c
Fabian Baena <fabian.baena@samanagroup.co>
2022-08-25 10:47:05 -0400

    add container management

--- nagios/scripts/etcd-init.sh
+++ nagios/scripts/etcd-init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-etcdctl set /samanamonitor/config/global \
-    '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application"]}'
-etcdctl set /samanamonitor/config/storefront-example \
-    '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application", "Citrix Delivery Services"]}'
+if [ -z "$1" ]; then
+    etcdctl set /samanamonitor/config/global \
+        '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application"]}'
+    etcdctl set /samanamonitor/config/storefront-example \
+        '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application", "Citrix Delivery Services"]}'
+else
+    docker exec -it $1 etcdctl set /samanamonitor/config/global \
+        '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application"]}'
+    docker exec -it $1 etcdctl set /samanamonitor/config/storefront-example \
+        '{"eventminutes":10,"eventmax":11,"eventlevelmax":3,"eventlist":["System","Application", "Citrix Delivery Services"]}'
+fi
