e93c9e4923ad1135feb51a9b9e4bd724af9d4aa3
Fabian Baena <fabian.baena@samanagroup.co>
2022-09-19 09:21:52 -0400

    added file

---
+++ nagios/winsrc/sammctxcloud.ps1
@@ -0,0 +1,79 @@
+$XDCredentialsProfileName='%(XDCredentialsProfileName)s'
+$EtcdServer='%(EtcdServer)s'
+$EtcdPort='%(EtcdPort)s'
+[string]$BasePath="/samanamonitor/ctx_data"
+[int]$MaxRecords=250
+[int]$Ttl=360
+[int]$DebugLevel=0
+[Boolean]$Timing=$True
+
+
+asnp Citrix.Broker.Admin.V2
+Get-XDAuthentication -ProfileName $XDCredentialsProfileName
+
+Function Epoch {
+    return $(Get-Date(Get-Date).ToUniversalTime()-uformat "%s")
+}
+Function UrlEncode {
+    param([string]$s)
+    return [System.web.HttpUtility]::UrlEncode($s)
+}
+
+
+$init_time=Epoch
+try {
+    $p=Get-XDCredentials -ProfileName $XDCredentialsProfileName
+    Get-XDAuthentication -ProfileName $XDCredentialsProfileName
+} catch {
+    Log -Level 0 -Message "Credential Profile cannot be found. Aborting."
+    return
+}
+$customerId=$p.Credentials.CustomerId
+$sitename=(Get-BrokerSite).Name
+$init_time = [Math]::Round((Epoch) - $init_time, 3)
+
+$customerpath="$BasePath/$CustomerId"
+$sitepath="$customerpath/$SiteName"
+
+$lastuid=0
+$allmachines=@()
+$download_time = Epoch
+while($true) {
+    $m=get-brokermachine -MaxRecordCount $MaxRecords -SortBy Uid -Filter {Uid -gt $lastUid}
+    if ($m.Length -eq 0 ) {
+        break
+    }
+    $lastuid = $m[-1].Uid
+    $allmachines += $m
+}
+$download_time = [Math]::Round((Epoch) - $download_time, 3)
+
+$json_time = Epoch
+$j = ConvertTo-Json $allmachines
+$json_time = [Math]::Round( (Epoch) - $json_time, 3)
+
+$compression_time = Epoch
+$Data= foreach ($c in $j.ToCharArray()) {
+        $c -as [Byte]
+}
+$ms = New-Object IO.MemoryStream
+$cs = New-Object System.IO.Compression.GZipStream ($ms, [Io.Compression.CompressionMode]"Compress")
+$cs.Write($Data, 0, $Data.Length)
+$cs.Close()
+
+$a=[Convert]::ToBase64String($ms.ToArray())
+"Total data compressed = $($a.Length)" | Write-Host
+$ms.Close()
+$compression_time = [Math]::Round( (Epoch) - $compression_time, 3)
+
+$upload_time = Epoch
+$res = Invoke-WebRequest -Uri "http://$($EtcdServer):$($EtcdPort)/v2/keys$sitepath/raw" `
+    -Method Put -Body @{value=$a;ttl=$Ttl} `
+    -ContentType "application/x-www-form-urlencoded"
+$upload_time = [Math]::Round( (Epoch) - $upload_time, 3)
+"Location: $($sitepath)/raw" | Write-Host -NoNewline
+
+" | totaldata=$($j.Length);;;; compressed=$($a.Length);;;; " + `
+    "init_time=$init_time;;;; download_time=$download_time;;;; " + `
+    "json_time=$json_time;;;; compression_time=$compression_time;;;; " + `
+    "upload_time=$upload_time;;;;" | Write-Host
