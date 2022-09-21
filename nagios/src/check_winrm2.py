4170f80d86b48d332b7ab69b8731859d0d5a7be2
Fabian Baena <fabian.baena@samanagroup.co>
2022-09-21 10:36:42 -0400

    encode variables before script concatenated

--- nagios/src/check_winrm2.py
+++ nagios/src/check_winrm2.py
@@ -58,7 +58,7 @@ class WinRMScript:
                 username=self.username,
                 password=self.password)
             shell_id = p.open_shell()
-            encoded_ps = b64encode((script % {'variables': variables}).encode('utf_16_le')).decode('ascii')
+            encoded_ps = b64encode((variables + script).encode('utf_16_le')).decode('ascii')
             command_id = p.run_command(shell_id, 'powershell', ['-encodedcommand {0}'.format(encoded_ps), ])
             std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
             self.check_error(std_err)
