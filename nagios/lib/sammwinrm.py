6a063266f6863e80b4ce37c96a030ee84c367235
Fabian Baena <fabian.baena@samanagroup.co>
2022-09-29 17:45:15 -0400

    added file

--- nagios/lib/sammwinrm.py
+++ nagios/lib/sammwinrm.py
@@ -318,6 +318,7 @@ class WMIQuery(WinRMCommand):
     def xmltodict(self, data_root, class_name, xmlns):
         data = {}
         for i in data_root.findall('./%s' % class_name, xmlns):
+            print(i)
             tagname = i.tag.replace('{'+self.base_uri+class_name+'}', '')
             if i.attrib.get(nil, 'false') == 'true':
                 data[tagname] = None
