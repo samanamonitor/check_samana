10765f9f27a20c62fc97854ff2f53763050dcba0
Fabian Baena <fabian.baena@samanagroup.co>
2022-10-03 12:15:44 -0400

    make python2 compatible

--- nagios/src/check_wmi3.py
+++ nagios/src/check_wmi3.py
@@ -55,6 +55,7 @@ def query_server(host, username, password, domain, namespace="root\\cimv2", filt
         if the key doesn't exist, then an empty tuple is expected, otherwise the tuple
         must contain all the arguments needed to complete the string
     '''
+    server = {}
     shell = WinRMShell()
     shell.open(host, {'username': username, 'password': password, 'domain': domain})
     if shell.connected != True:
