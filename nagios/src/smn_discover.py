22805de10d378b3bafc9587921c5249fbed2cb58
Fabian Baena <fabian.baena@samanagroup.co>
2022-10-12 10:23:39 -0400

    continue adding code

--- nagios/src/smn_discover.py
+++ nagios/src/smn_discover.py
@@ -29,13 +29,14 @@ def auto_init():
 
 
 def remove_hosts(ddc_name):
-    hosts = c.get("/samanamonitor/ctx_data/%s/hosts" % ddc_name)
+    hosts = c.get("/samanamonitor/ctx_data/%s/computer" % ddc_name)
     auto=auto_init()
     auto_hosts = auto.get_effective_hosts()
     ctx_hosts=[]
     for h in hosts.leaves:
         try:
-            hostdata = json.loads(h.value)
+            hd = c.get("%s/data" % h.key)
+            hostdata = json.loads(hd.value)
         except:
             continue
         host_name = "%s-A" % hostdata['DNSName'].split('.')[0].upper()
@@ -66,9 +67,7 @@ def add_ctx_hg(hg_name, alias):
         autohg.save()
     return dg_hg
 
-def new_ctx_host(hostdata, role, method="fqdn"):
-    if "SAMM_Remove" in hostdata['Tags']:
-        return None
+def new_ctx_host(hostdata, role, ddc_name, method="fqdn"):
     hg_alias = hostdata['DesktopGroupName'].lower()
     hg_name = "ctx-%s" % hg_alias.replace(' ', '-')
     add_ctx_hg(hg_name, hg_alias)
@@ -116,6 +115,97 @@ def get_farm_db():
     data="Server=smnnovctxddc1\\sqlexpress;Initial Catalog=CitrixSAMANASite;Integrated Security=True"
     data.split(';')[0].split('=')[1]
 
+POWERSTATE_UNMANAGED    = 0
+POWERSTATE_UNKNOWN      = 1
+POWERSTATE_UNAVAILABLE  = 2
+POWERSTATE_OFF          = 3
+POWERSTATE_ON           = 4
+POWERSTATE_SUSPENDED    = 5
+POWERSTATE_TURNINGON    = 6
+POWERSTATE_TURNINGOFF   = 7
+POWERSTATE_SUSPENDING   = 8
+POWERSTATE_RESUMING     = 9
+POWERSTATE_NOTSUPPORTED = 10
+
+def get_hosts_in_ctx_site(ddc_name):
+    '''Returns a dict that conatins hosts in the Citrix Site
+        Dict is started with two keys with empty sets: all and disable
+        The tags defined in each host is validated for SAMM_ tags
+        and keys are created for each of these tags
+    '''
+    ctx_hosts={'all': set(), 'disable': set(), 'remove': set()}
+    try:
+        hosts = c.get("/samanamonitor/ctx_data/%s/computer" % ddc_name)
+    except Exception:
+        return []
+    for i in hosts.leaves:
+        try:
+            hdatajson = c.get("%s/data" % i.key)
+            hdata = json.loads(hdatajson.value)
+        except Exception as e:
+            pass
+        fqdn = i.key.split("/")[-1]
+        ctx_hosts['all'].add(fqdn)
+        for t in hdata['Tags']:
+            tag= t.lower()
+            if tag[:5] != "samm_": continue
+            sammtag = tag[5:]
+            if sammtag not in ctx_hosts:
+                ctx_hosts[sammtag] = set()
+            ctx_hosts[sammtag].add(fqdn)
+        if hdata['PowerState'] == POWERSTATE_OFF \
+            or hdata['PowerState'] == POWERSTATE_TURNINGOFF \
+            or hdata['PowerState'] == POWERSTATE_RESUMING \
+            or hdata['PowerState'] == POWERSTATE_TURNINGON \
+            or hdata['PowerState'] == POWERSTATE_SUSPENDED \
+            or hdata['PowerState'] == POWERSTATE_SUSPENDING:
+            ctx_hosts['disable'].add(fqdn)
+    return ctx_hosts
+
+def get_hosts_in_samm(ddc_name):
+    '''Returns a dict that conatins hosts in SAMM
+        Dict is started with two keys with empty sets: all and disable
+    '''
+    hg=Hostgroup.objects.get_by_shortname("autohostgroup")
+    ahg=hg.get_effective_hosts()
+    samm_ctx_hosts={'all': set(), 'remove': set()}
+    for i in ahg:
+        ddc=i.get_macro("$_HOSTHOME_DDC$")
+        if ddc == ddc_name:
+            fqdn = i.get_macro("$_HOSTID$")
+            samm_ctx_hosts['all'].add(fqdn)
+            if i.register == 0:
+                samm_ctx_hosts['remove'].add(fqdn)
+    return samm_ctx_hosts
+
+def get_name(data):
+    name = ""
+    return name
+
+def remove_hosts(hosts):
+    for h in hosts:
+
+def process_hosts(ddc_name):
+    '''Receives a DDC name, collects the hosts from the farm and
+        the hosts configured in SAMM and calculates which hosts
+        need to be disabled, unregistered or removed from SAMM
+    '''
+    change = False
+    ctx_site_hosts=get_hosts_in_ctx_site(ddc_name)
+    samm_ctx_hosts=get_hosts_in_samm(ddc_name)
+    hosts_to_remove     = samm_ctx_hosts['all'] - ctx_site_hosts['all']
+    change |= remove_hosts(hosts_to_remove)
+    hosts_to_add        = ctx_site_hosts['all'] - samm_ctx_hosts['all']
+    change |= add_hosts(hosts_to_add)
+    hosts_to_unregister = ctx_site_hosts['remove']
+    change |= unregister_hosts(hosts_to_unregister)
+    hosts_to_register   = ctx_site_hosts['all'] - ctx_site_hosts['remove']
+    change |= unregister_hosts(hosts_to_register)
+    hosts_to_disable    = ctx_site_hosts['disable']
+    disable_hosts(hosts_to_disable)
+    hosts_to_enable     = ctx_site_hosts['all'] - ctx_site_hosts['disable']
+    enable_hosts(hosts_to_enable)
+    return change
 
 def main(argv):
     pass
