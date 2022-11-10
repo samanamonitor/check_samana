0999c465a37bf3468160005fa5152fae03f891c6
Fabian Baena <fabbaena@gmail.com>
2022-11-10 11:23:04 -0500

    added test

--- nagios/lib/sammcheck/test.py
+++ nagios/lib/sammcheck/test.py
@@ -3,11 +3,11 @@ import importlib
 class SAMMTest:
     plugins = [
         {
-            name: "check_samana4",
-            plugin: importlib.import_module(".etcdcheck.SAMMEtcdCheck")
+            "name": "check_samana4",
+            "plugin": importlib.import_module(".etcdcheck.SAMMEtcdCheck")
         }, {
-            name: "check_wmi3",
-            plugin: importlib.import_module(".wmicheck.SAMMWMICheck")
+            "name": "check_wmi3",
+            "plugin": importlib.import_module(".wmicheck.SAMMWMICheck")
         }]
 
     def __init__(self):
