63934ee93c0d7be076ee6298f94d664dae469d3a
Fabian Baena <fabbaena@gmail.com>
2023-02-23 10:30:47 -0500

    added counter check

---
+++ nagios/src/check_nutanix_nicerror2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+HOST_ID=$1
+INTERFACES=$2
+WARNVAL=$3
+CRITVAL=$4
+
+STATUS="OK"
+RETVAL=0
+ifnames=(${INTERFACES//,/ })
+sshconfig=/home/nagios/.ssh/config
+sshkey=/home/nagios/.ssh/id_rsa
+
+if ! grep -q ${HOST_ID} ${sshconfig}; then
+    echo "UNKNOWN - Host ${HOST_ID} not configured for ssh." >&2
+    exit 3
+fi
+
+CURVALS=(0 $(ssh -F ${sshconfig} -i ${sshkey} ${HOST_ID} \
+    "for i in ${INTERFACES//,/ }; do cat /sys/class/net/\$i/statistics/rx_errors; done" 2>/dev/null))
+
+if [ "$?" != "0" ] || [ -z "${CURVALS[1]}" ] ; then
+    echo "UNKNOWN - Error executing check"
+    echo "${CURVALS[@]}"
+    exit 3
+fi
+
+printf "OK |"
+
+for i in $(seq ${#ifnames[@]}); do
+    printf " %s=%s;;;;" ${ifnames[$i]} ${CURVALS[$i]}
+done
+printf "\n"
+exit ${RETVAL}
